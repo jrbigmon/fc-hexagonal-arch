c1ccbf28a248ed7bfbeac2c596bead63bc1485fb
jrbigmon <juniorterriaga0@gmail.com>
2024-07-23 22:55:02 -0300

    feat: implements another validations to product

--- src/application/product.spec.ts
+++ src/application/product.spec.ts
@@ -3,7 +3,7 @@ import { randomUUID } from "crypto";
 
 describe("Application Product entity", () => {
   describe("IsValid", () => {
-    it("Should be create a new product", () => {
+    it("Product is valid", () => {
       const product = new Product(
         randomUUID(),
         "Product A",
@@ -14,6 +14,78 @@ describe("Application Product entity", () => {
       expect(() => {
         product.isValid();
       }).not.toThrow();
+    });
+
+    it("Product is not valid when status is enabled and price is less than zero", () => {
+      const product = new Product(
+        randomUUID(),
+        "Product A",
+        ProductStatus.ENABLED,
+        0
+      );
+
+      expect(() => {
+        product.isValid();
+      }).toThrow(
+        "Product status is enabled but the price is less or equal to zero"
+      );
+    });
+
+    it("Product is not valid when status is disabled and price is greater than zero", () => {
+      const product = new Product(
+        randomUUID(),
+        "Product A",
+        ProductStatus.DISABLED,
+        120
+      );
+
+      expect(() => {
+        product.isValid();
+      }).toThrow(
+        "Product status is disabled but the price is greater than zero"
+      );
+    });
+
+    it("Product is not valid when status is not supported", () => {
+      const product = new Product(
+        randomUUID(),
+        "Product A",
+        "Status not supported" as ProductStatus,
+        120
+      );
+
+      expect(() => {
+        product.isValid();
+      }).toThrow("Product status is not supported");
+    });
+
+    it("Product is not valid when it has no id", () => {
+      const product = new Product("", "Product A", ProductStatus.ENABLED, 120);
+
+      expect(() => {
+        product.isValid();
+      }).toThrow("Product id is required");
+    });
+
+    it("Product is not valid when it has no name", () => {
+      const product = new Product(randomUUID(), "", ProductStatus.ENABLED, 120);
+
+      expect(() => {
+        product.isValid();
+      }).toThrow("Product name is required");
+    });
+
+    it("Product is not valid when it has no valid price", () => {
+      const product = new Product(
+        randomUUID(),
+        "Product A",
+        ProductStatus.ENABLED,
+        "anyPriceInvalid" as unknown as number
+      );
+
+      expect(() => {
+        product.isValid();
+      }).toThrow("Product price is required and must be a number");
     });
   });
 
--- src/application/product.ts
+++ src/application/product.ts
@@ -35,8 +35,39 @@ export class Product implements ProductInterface {
       );
     }
 
+    if (this.status === ProductStatus.DISABLED && this.price > 0) {
+      throw new Error(
+        "Product status is disabled but the price is greater than zero"
+      );
+    }
+
+    if (
+      this.status !== ProductStatus.DISABLED &&
+      this.status !== ProductStatus.ENABLED
+    ) {
+      throw new Error("Product status is not supported");
+    }
+
+    if (!this.id) {
+      throw new Error("Product id is required");
+    }
+
+    if (!this.name) {
+      throw new Error("Product name is required");
+    }
+
+    if (
+      this.price === null ||
+      this.price === undefined ||
+      typeof this.price !== "number" ||
+      isNaN(Number(this.price))
+    ) {
+      throw new Error("Product price is required and must be a number");
+    }
+
     return true;
   }
+
   enable(): void {
     if (this.price > 0) {
       this.status = ProductStatus.ENABLED;
@@ -44,6 +75,7 @@ export class Product implements ProductInterface {
       throw new Error("Price must be greater than zero to enable the product.");
     }
   }
+
   disable(): void {
     if (this.price === 0) {
       this.status = ProductStatus.DISABLED;
@@ -51,21 +83,27 @@ export class Product implements ProductInterface {
       throw new Error("Price must be zero to disable the product.");
     }
   }
+
   getStatus(): string {
     return this.status;
   }
+
   getId(): string {
     return this.id;
   }
+
   getName(): string {
     return this.name;
   }
+
   setName(name: string): void {
     this.name = name;
   }
+
   getPrice(): number {
     return this.price;
   }
+
   setPrice(price: number): void {
     this.price = price;
   }
